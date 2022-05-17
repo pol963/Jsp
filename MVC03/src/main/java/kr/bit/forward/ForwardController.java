f099621392609497fe35a52a55b0030da5e7c1d8
pol963 <pol963@naver.com>
2022-05-18 05:14:36 +0900

    22_05_18_01

---
+++ MVC03/src/main/java/kr/bit/forward/ForwardController.java
@@ -0,0 +1,28 @@
+package kr.bit.forward;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/fc.do")
+public class ForwardController extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//가정 -> 클라이언트에서 fc.do라는 요청을 하였고 fc.do요청을 받은 컨트롤러는 view에 데이터를 넘겨준뒤 result.jsp페이지에서 출력하는 예제.
+		
+		// 1. forward 실습. 
+		String data = "apple";
+		
+		//Controller에서 View페이지로 전환하는 방법.
+		// 1. redirect 
+		// 2. forward
+		response.sendRedirect("");
+		
+		
+		
+	}
+
+}
