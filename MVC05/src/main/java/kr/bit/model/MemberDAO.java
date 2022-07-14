f6be3573dc921e47891394d7ad7953b7ac1412f8
pol963 <pol963@naver.com>
2022-07-15 04:23:38 +0900

    22-07-15-01

--- MVC05/src/main/java/kr/bit/model/MemberDAO.java
+++ MVC05/src/main/java/kr/bit/model/MemberDAO.java
@@ -14,12 +14,21 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 public class MemberDAO {
 
+	//Connection Pool기법을 이용하여 DB와 연동해서 요청을 처리하기 위한 변수 선언.
+	//SqlSessionFactory자료형을 변수를 선언 -> 
 	private static SqlSessionFactory sqlSessionFactory;
 	
-	static { //초기화 블럭 프로그램 시작후 딱 한번만 실행되는 코드.
+	//초기화 블럭 프로그램 시작후 딱 한번만 실행되는 코드.
+	
+	static { 
+		
 		try {
+			//환경설정파일을 가르키는 경로.
 		String resource = "kr/bit/mybatis/config.xml";
+		//설정읽어들이기.
 		InputStream inputStream = Resources.getResourceAsStream(resource); //읽기
+		
+		//sqlSession을 담고있는 Factory메모리공간을 가르키는 객체생성. -> build메서드를 이용하여 설정을 읽어드린걸로 객체생성.
 		sqlSessionFactory =  new SqlSessionFactoryBuilder().build(inputStream);
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -30,10 +39,14 @@ public class MemberDAO {
 	//회원리스트 전체보기.
 	public List<MemberVO> memberList() {
 		
-		//Connection+statment
+		//[Connection+statment] = SqlSession.
+		//아래코드는 즉 sqlSessionFactory에서 sqlSession을 꺼내와서 session에 담아준것.
 		SqlSession session = sqlSessionFactory.openSession();
+		
 		//selectList메소드가 vo로 묶고 ArrayList로 다시 묶어주는것을 다해줍니다. 타입은 지정.
+		//selectList메서드 -> 여러개를 선택하여 리턴받아올 수 있는 메서드. -> 기본적으로 List를 리턴합니다.
 		List<MemberVO> list = session.selectList("memberList"); 
+		//DB작업이 끝나면 close하여 반납을 해줘야 합니다. sqlSession을 SqlSessionFactory에 반납.
 		session.close();
 		return list;
 		
