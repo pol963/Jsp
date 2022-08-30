0b6bcd29d20ab4ab69af151a55556d050c955366
pol963 <pol963@naver.com>
2022-08-30 19:35:59 +0900

    22-08-30-01

--- SpringMVC01/src/main/java/kr/narp/myapp/MemberController.java
+++ SpringMVC01/src/main/java/kr/narp/myapp/MemberController.java
@@ -1,7 +1,10 @@
 package kr.narp.myapp;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//어노테어션(@)이 있다면 전처리가 먼저 됩니다.-> Spring Container
+@Controller
 public class MemberController {
 
 	//개별로 어노테이션으로 요청이왔을시에 처리.
